f43ee4ecb9aec08861af8881913fa3b55549644a
loiserundi <loise.rundi@students.braeside.ac.ke>
2024-08-28 20:23:54 +0300

    operators

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("Hello")
+num1 = 45
+num2 = 3
+
+print("Number1", num1)
+print("Number2", num2)
+print("addition: ", num1 + num2)
+print("subtraction: ", num1 - num2)
+print("product: ", num1 * num2)
+print("division: ", num1 / num2)
+print("floor division: ", num1 // num2)
+print("modulus operation: ", num1 % num2)
+print("square: ", num2**2)
+print("square root: ", num1**0.5)
+
+print("equal to: ", num1 == num2)
+print("Number 1 greater than Number 2: ", num1 > num2)
+print("Number 2 greater than Number 1: ", num1 < num2)
+print("Not equal to: ", num1 != num2)
+
+result = num1 / 2 + num2**2 + 10
+print("Result of given equation is: ", result)
